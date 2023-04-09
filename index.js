b84ed8835c2aeabc5b6b20463b9855cc7e3724b8
GodoyMS <youremail@domain.com>
2023-04-09 18:01:23 -0500

    10

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dotenv=require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const app=express();
 app.use(cors({
-        origin:'http://localhost:3000',
+        origin:'http://localhost:3000/',
         credentials: true,
         optionsSuccessStatus: 200,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']

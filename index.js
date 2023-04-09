9238551ac5efef9cbd025b16cf85d1abc8a5cce2
GodoyMS <youremail@domain.com>
2023-04-09 15:08:06 -0500

    changed cors

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors=require('cors');
 const dotenv=require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const app=express();
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
